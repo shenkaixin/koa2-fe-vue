4b60b7eb30be0a5315c01d0fe86507409be113bf
MeiYU <mieyu@10.qq.com>
2019-02-02 15:16:15 +0800

    完成文件上传的demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <el-upload
+      action="/api//uploadFile"
+    >
+      <el-button size="small" type="primary">点击上传</el-button>
+    </el-upload>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+</style>
